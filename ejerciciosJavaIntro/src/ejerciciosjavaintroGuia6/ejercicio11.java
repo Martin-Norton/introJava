33f4048a9ecd24d7f080d0939d8c2a205d604d4e
Martin Norton <martinnorton07@gmail.com>
2022-11-08 20:00:40 -0300

    ejercicio11

---
+++ ejerciciosJavaIntro/src/ejerciciosjavaintroGuia6/ejercicio11.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjavaintroGuia6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Martin Norton
+ */
+public class ejercicio11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+        System.out.println("ingrese una frase terminada en punto");
+        String frase = leer.nextLine();
+        int largo = frase.length();
+        String newFrase="", newFrase2="";
+        for (int i = 0; i < largo; i++) {
+          switch(frase.substring(i,i+1)){
+              case "a": 
+                  newFrase2 += "@";
+                  break;
+               case "e": 
+                  newFrase2 += "#";
+                  break;
+               case "i": 
+                  newFrase2 += "$";
+                  break;
+               case "o": 
+                  newFrase2 += "%";
+                  break;
+               case "u": 
+                  newFrase2 += "*";
+                  break;  
+               default : 
+                  newFrase2 += frase.substring(i,i+1);
+        }                            
+    }
+         System.out.println(newFrase2);
+}
+}
