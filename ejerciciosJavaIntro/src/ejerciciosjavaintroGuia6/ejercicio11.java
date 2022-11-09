b2096b6647f257ec1f51908e0c59a2d16da575c2
Martin Norton <martinnorton07@gmail.com>
2022-11-08 21:46:38 -0300

    ej11

--- ejerciciosJavaIntro/src/ejerciciosjavaintroGuia6/ejercicio11.java
+++ ejerciciosJavaIntro/src/ejerciciosjavaintroGuia6/ejercicio11.java
@@ -18,31 +18,52 @@ public class ejercicio11 {
      */
     public static void main(String[] args) {
         Scanner leer=new Scanner(System.in);
-        System.out.println("ingrese una frase terminada en punto");
-        String frase = leer.nextLine();
-        int largo = frase.length();
-        String newFrase="", newFrase2="";
-        for (int i = 0; i < largo; i++) {
-          switch(frase.substring(i,i+1)){
-              case "a": 
-                  newFrase2 += "@";
-                  break;
-               case "e": 
-                  newFrase2 += "#";
-                  break;
-               case "i": 
-                  newFrase2 += "$";
-                  break;
-               case "o": 
-                  newFrase2 += "%";
-                  break;
-               case "u": 
-                  newFrase2 += "*";
-                  break;  
-               default : 
-                  newFrase2 += frase.substring(i,i+1);
-        }                            
-    }
-         System.out.println(newFrase2);
+        System.out.println("ingrese dos numeros enteros:");
+        int num1 = leer.nextInt();
+        int num2 = leer.nextInt();
+        String letra = "n";
+        
+        do {
+            System.out.println("ingrese una opcion");
+        System.out.println("MENU");
+        System.out.println("1-sumar");
+        System.out.println("2-restar");
+        System.out.println("3-multiplicar");
+        System.out.println("4-dividir");
+        System.out.println("5-salir");
+       int opc = leer.nextInt();
+       int total;
+       switch (opc)  {
+           case 1:
+               total = num1 + num2;
+               System.out.println(total);
+               break;
+           case 2 :
+               total = num1 - num2;
+               System.out.println(total);
+               break;
+           case 3:
+               total= num1 * num2;
+               System.out.println(total);
+               break;
+           case 4: 
+               total= num1 / num2;
+               System.out.println(total);
+               break;
+           case 5:
+               letra= confirmar ();
+               break;
+}
+             } while (letra == "n");
+       
+        
+}
+    public static String confirmar () {
+            Scanner leer=new Scanner(System.in);
+            System.out.println("quiere salir? s/n");
+            String letra = leer.nextLine();
+        return letra;
+         
 }
 }
+ 
